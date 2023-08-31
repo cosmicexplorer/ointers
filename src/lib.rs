d88224a84fd9f74919844a85a28705b358f4873e
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-08-31 17:49:33 -0400

    apply rustfmt with a rustfmt.toml

--- src/lib.rs
+++ src/lib.rs
@@ -108,18 +108,20 @@
 #![no_std]
 #![allow(unstable_name_collisions)]
 
-#[cfg(feature="alloc")]
+#[cfg(feature = "alloc")]
 extern crate alloc;
-#[cfg(feature="alloc")]
+#[cfg(feature = "alloc")]
 use alloc::boxed::Box;
 
-use core::hash::*;
-use core::marker::PhantomData;
-use core::mem::align_of;
-use core::ops::{Deref, DerefMut};
-use core::ptr::NonNull;
+use core::{
+  hash::*,
+  marker::PhantomData,
+  mem::align_of,
+  ops::{Deref, DerefMut},
+  ptr::NonNull,
+};
 
-#[cfg(not(feature="sptr"))]
+#[cfg(not(feature = "sptr"))]
 mod sptr;
 
 use sptr::Strict as _;
@@ -138,9 +140,7 @@ pub struct Ointer<T, const A: u8, const S: bool, const V: u8> {
 }
 
 impl<T, const A: u8, const S: bool, const V: u8> Hash for Ointer<T, A, S, V> {
-  fn hash<H: Hasher>(&self, state: &mut H) {
-    self.ptr.hash(state)
-  }
+  fn hash<H: Hasher>(&self, state: &mut H) { self.ptr.hash(state) }
 }
 
 impl<T, const A: u8, const S: bool, const V: u8> PartialEq<Self> for Ointer<T, A, S, V> {
@@ -159,7 +159,7 @@ impl<T, const A: u8, const S: bool, const V: u8> Ointer<T, A, S, V> {
   /// Creates a new Ointer from a presumed legitimate pointer.
   ///
   /// # Safety
-  /// 
+  ///
   /// * T's alignment must enable stealing A bits.
   /// * The high bits (sign upwards) must match a stack pointer's high bits.
   /// * If compiling for a 64bit arch, V must be at most 25.
@@ -168,7 +168,9 @@ impl<T, const A: u8, const S: bool, const V: u8> Ointer<T, A, S, V> {
   /// These invariants are checked with `debug_assert` only, hence
   /// `unsafe`. The usual caveats of pointers apply.
   pub unsafe fn new(ptr: *mut T) -> Self {
-    Ointer { ptr: pack(ptr, A, S, V) }
+    Ointer {
+      ptr: pack(ptr, A, S, V),
+    }
   }
 
   /// Constructor that enables stealing bits.
@@ -189,19 +191,18 @@ impl<T, const A: u8, const S: bool, const V: u8> Ointer<T, A, S, V> {
   /// steals them from the ointer.
   pub fn steal(self, bits: usize) -> Self {
     let mask = asv_mask(A, S, V);
-    let ptr = self.ptr.with_addr((bits & mask) | (self.ptr.addr() & !mask));
+    let ptr = self
+      .ptr
+      .with_addr((bits & mask) | (self.ptr.addr() & !mask));
     Self { ptr }
   }
 
   /// Get the pointer without the stolen bits
-  pub fn as_ptr(self) -> *mut T {
-    unsafe { unpack(self.ptr, A, S, V) }
-  }
+  pub fn as_ptr(self) -> *mut T { unsafe { unpack(self.ptr, A, S, V) } }
 
   /// Direct access to the underlying data. The pointer it returns
   /// may not be valid.
   pub fn raw(self) -> usize { self.ptr.expose_addr() }
-
 }
 
 /// A non-null pointer that we stole the high bits off.
@@ -226,9 +227,7 @@ impl<T, const A: u8, const S: bool, const V: u8> PartialEq<Self> for NotNull<T, 
 impl<T, const A: u8, const S: bool, const V: u8> Eq for NotNull<T, A, S, V> {}
 
 impl<T, const A: u8, const S: bool, const V: u8> Hash for NotNull<T, A, S, V> {
-  fn hash<H: Hasher>(&self, state: &mut H) {
-    self.0.hash(state)
-  }
+  fn hash<H: Hasher>(&self, state: &mut H) { self.0.hash(state) }
 }
 
 impl<T: Sized, const A: u8, const S: bool, const V: u8> NotNull<T, A, S, V> {
@@ -269,7 +268,10 @@ impl<T: Sized, const A: u8, const S: bool, const V: u8> NotNull<T, A, S, V> {
     let bits = bits & mask;
     let ptr = self.0.as_ptr();
     let addr = ptr.addr() & !mask;
-    Self(unsafe { NonNull::new_unchecked(ptr.with_addr(addr | bits)) }, PhantomData)
+    Self(
+      unsafe { NonNull::new_unchecked(ptr.with_addr(addr | bits)) },
+      PhantomData,
+    )
   }
 
   /// Get the pointer without the stolen bits
@@ -280,7 +282,6 @@ impl<T: Sized, const A: u8, const S: bool, const V: u8> NotNull<T, A, S, V> {
   /// Direct access to the underlying data. The pointer it returns
   /// may not be valid.
   pub fn raw(self) -> usize { self.0.as_ptr().expose_addr() }
-
 }
 
 /// A Box that we stole the high bits off.
@@ -290,10 +291,10 @@ impl<T: Sized, const A: u8, const S: bool, const V: u8> NotNull<T, A, S, V> {
 /// A: number of bits to steal based on the alignment requirements of T.
 #[derive(Debug)]
 #[repr(transparent)]
-#[cfg(feature="alloc")]
+#[cfg(feature = "alloc")]
 pub struct Ox<T, const A: u8, const S: bool, const V: u8>(NonNull<u8>, PhantomData<T>);
 
-#[cfg(feature="alloc")]
+#[cfg(feature = "alloc")]
 impl<T, const A: u8, const S: bool, const V: u8> Clone for Ox<T, A, S, V> {
   fn clone(&self) -> Self { Ox(self.0, PhantomData) }
 }
@@ -305,13 +306,11 @@ impl<T, const A: u8, const S: bool, const V: u8> PartialEq<Self> for Ox<T, A, S,
 impl<T, const A: u8, const S: bool, const V: u8> Eq for Ox<T, A, S, V> {}
 
 impl<T, const A: u8, const S: bool, const V: u8> Hash for Ox<T, A, S, V> {
-  fn hash<H: Hasher>(&self, state: &mut H) {
-    self.0.hash(state)
-  }
+  fn hash<H: Hasher>(&self, state: &mut H) { self.0.hash(state) }
 }
 
 
-#[cfg(feature="alloc")]
+#[cfg(feature = "alloc")]
 impl<T, const A: u8, const S: bool, const V: u8> Ox<T, A, S, V> {
   /// Creates a new Ox from a box.
   ///
@@ -355,9 +354,7 @@ impl<T, const A: u8, const S: bool, const V: u8> Ox<T, A, S, V> {
   }
 
   /// Get the box back without the stolen bits
-  pub fn into_box(self) -> Box<T> {
-    unsafe { Box::from_raw(unpack(self.as_ptr(), A, S, V)) }
-  }
+  pub fn into_box(self) -> Box<T> { unsafe { Box::from_raw(unpack(self.as_ptr(), A, S, V)) } }
 
   /// Get the box back without the stolen bits
   pub fn as_ptr(&self) -> *mut T { self.0.as_ptr().cast() }
@@ -367,31 +364,26 @@ impl<T, const A: u8, const S: bool, const V: u8> Ox<T, A, S, V> {
   pub fn raw(&self) -> usize { self.0.as_ptr().expose_addr() }
 }
 
-#[cfg(feature="alloc")]
+#[cfg(feature = "alloc")]
 impl<T, const A: u8, const S: bool, const V: u8> Deref for Ox<T, A, S, V> {
   type Target = T;
-  fn deref(&self) -> &T {
-    unsafe { &*self.0.as_ptr().cast() }
-  }
+
+  fn deref(&self) -> &T { unsafe { &*self.0.as_ptr().cast() } }
 }
 
-#[cfg(feature="alloc")]
+#[cfg(feature = "alloc")]
 impl<T, const A: u8, const S: bool, const V: u8> DerefMut for Ox<T, A, S, V> {
-  fn deref_mut(&mut self) -> &mut T {
-    unsafe { &mut *self.0.as_ptr().cast() }
-  }
+  fn deref_mut(&mut self) -> &mut T { unsafe { &mut *self.0.as_ptr().cast() } }
 }
 
-#[cfg(feature="alloc")]
+#[cfg(feature = "alloc")]
 impl<T, const A: u8, const S: bool, const V: u8> Drop for Ox<T, A, S, V> {
-  fn drop(&mut self) {
-    drop(unsafe { Box::from_raw(self.0.as_ptr()) })
-  }
+  fn drop(&mut self) { drop(unsafe { Box::from_raw(self.0.as_ptr()) }) }
 }
 
 
-
-/// Packs a pointer into the bottom `sizeof(usize) - (a + s + v)` bits of a usize.
+/// Packs a pointer into the bottom `sizeof(usize) - (a + s + v)` bits of a
+/// usize.
 ///
 /// # Safety
 ///
@@ -407,7 +399,10 @@ pub unsafe fn pack<T: Sized>(ptr: *mut T, a: u8, s: bool, v: u8) -> *mut T {
   #[cfg(debug_assertions)]
   {
     debug_assert!((1 << a) <= align_of::<T>());
-    #[cfg(all(not(target_pointer_width="64"), not(feature="i_know_what_im_doing")))]
+    #[cfg(all(
+      not(target_pointer_width = "64"),
+      not(feature = "i_know_what_im_doing")
+    ))]
     debug_assert!(v == 0);
     debug_assert!(v <= 25);
     // If S is set, the user has indicated they will never be
@@ -461,9 +456,13 @@ pub unsafe fn unpack<T: Sized>(packed: *mut T, a: u8, s: bool, v: u8) -> *mut T 
 pub const fn asv_mask(a: u8, s: bool, v: u8) -> usize { mask(a + s as u8 + v) }
 
 /// Produces a mask where the stolen bits (at the top) are set
-pub const fn mask(bits: u8) -> usize { (isize::MIN >> (max(bits as usize,1) - 1)) as usize }
+pub const fn mask(bits: u8) -> usize { (isize::MIN >> (max(bits as usize, 1) - 1)) as usize }
 
 // core::cmp::max and usize::max aren't const fns
 const fn max(x: usize, y: usize) -> usize {
-  if x <= y { y } else { x }
+  if x <= y {
+    y
+  } else {
+    x
+  }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,26 +1,30 @@
+use ointers::*;
+use rand::{rngs::ThreadRng, thread_rng, RngCore};
+use rayon::prelude::*;
 use std::fmt::Debug;
 use std::ptr::NonNull;
-use ointers::*;
-use rand::{thread_rng, rngs::ThreadRng, RngCore};
-use rayon::prelude::*;
-    
-#[cfg(target_pointer_width="16")]
+
+#[cfg(target_pointer_width = "16")]
 fn random_usize(rng: &mut ThreadRng) -> usize {
     let mut bytes = [0u8; 2];
     rng.fill_bytes(&bytes[..]);
     usize::from_native_bytes(bytes)
 }
 
-#[cfg(target_pointer_width="32")]
-fn random_usize(rng: &mut ThreadRng) -> usize { rng.next_u32() as usize }
+#[cfg(target_pointer_width = "32")]
+fn random_usize(rng: &mut ThreadRng) -> usize {
+    rng.next_u32() as usize
+}
 
-#[cfg(target_pointer_width="64")]
-fn random_usize(rng: &mut ThreadRng) -> usize { rng.next_u64() as usize }
+#[cfg(target_pointer_width = "64")]
+fn random_usize(rng: &mut ThreadRng) -> usize {
+    rng.next_u64() as usize
+}
 
 fn round_trip_ointer<T: Copy + Debug + Eq, const A: u8, const S: bool, const V: u8>(
     ptr: *mut T,
     val: T,
-    theft: usize
+    theft: usize,
 ) {
     let mask = mask(A + S as u8 + V);
     let stolen = theft & mask;
@@ -53,7 +57,7 @@ fn round_trip_ointer<T: Copy + Debug + Eq, const A: u8, const S: bool, const V: 
 fn round_trip_not_null<T: Copy + Debug + Eq, const A: u8, const S: bool, const V: u8>(
     ptr: NonNull<T>,
     val: T,
-    theft: usize
+    theft: usize,
 ) {
     let mask = mask(A + S as u8 + V);
     let stolen = theft & mask;
@@ -89,11 +93,11 @@ macro_rules! rt_ointer {
         round_trip_ointer::<$type, 1, false, $v>($ptr, $value, $theft);
         round_trip_ointer::<$type, 2, false, $v>($ptr, $value, $theft);
         round_trip_ointer::<$type, 3, false, $v>($ptr, $value, $theft);
-        round_trip_ointer::<$type, 0, true,  $v>($ptr, $value, $theft);
-        round_trip_ointer::<$type, 1, true,  $v>($ptr, $value, $theft);
-        round_trip_ointer::<$type, 2, true,  $v>($ptr, $value, $theft);
-        round_trip_ointer::<$type, 3, true,  $v>($ptr, $value, $theft);
-    }
+        round_trip_ointer::<$type, 0, true, $v>($ptr, $value, $theft);
+        round_trip_ointer::<$type, 1, true, $v>($ptr, $value, $theft);
+        round_trip_ointer::<$type, 2, true, $v>($ptr, $value, $theft);
+        round_trip_ointer::<$type, 3, true, $v>($ptr, $value, $theft);
+    };
 }
 
 macro_rules! rt_not_null {
@@ -102,11 +106,11 @@ macro_rules! rt_not_null {
         round_trip_not_null::<$type, 1, false, $v>($ptr, $value, $theft);
         round_trip_not_null::<$type, 2, false, $v>($ptr, $value, $theft);
         round_trip_not_null::<$type, 3, false, $v>($ptr, $value, $theft);
-        round_trip_not_null::<$type, 0, true,  $v>($ptr, $value, $theft);
-        round_trip_not_null::<$type, 1, true,  $v>($ptr, $value, $theft);
-        round_trip_not_null::<$type, 2, true,  $v>($ptr, $value, $theft);
-        round_trip_not_null::<$type, 3, true,  $v>($ptr, $value, $theft);
-    }
+        round_trip_not_null::<$type, 0, true, $v>($ptr, $value, $theft);
+        round_trip_not_null::<$type, 1, true, $v>($ptr, $value, $theft);
+        round_trip_not_null::<$type, 2, true, $v>($ptr, $value, $theft);
+        round_trip_not_null::<$type, 3, true, $v>($ptr, $value, $theft);
+    };
 }
 
 #[test]
@@ -116,22 +120,38 @@ fn round_trip_ointer_locals() {
         let mut a: usize = random_usize(&mut rng);
         let theft = random_usize(&mut rng);
         rt_ointer!(a, &mut a as &mut usize, theft, usize, 0);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 1);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 2);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 3);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 4);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 5);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 6);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 7);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 8);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 9);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 10);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 11);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 12);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 13);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 14);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 15);
-        #[cfg(target_pointer_width="64")] rt_ointer!(a, &mut a as *mut usize, theft, usize, 16);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 1);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 2);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 3);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 4);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 5);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 6);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 7);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 8);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 9);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 10);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 11);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 12);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 13);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 14);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 15);
+        #[cfg(target_pointer_width = "64")]
+        rt_ointer!(a, &mut a as *mut usize, theft, usize, 16);
     })
 }
 
@@ -143,22 +163,38 @@ fn round_trip_ointer_boxes() {
         let theft = random_usize(&mut rng);
         unsafe {
             rt_ointer!(a.read(), a, theft, usize, 0);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 1);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 2);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 3);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 4);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 5);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 6);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 7);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 8);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 9);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 10);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 11);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 12);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 13);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 14);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 15);
-            #[cfg(target_pointer_width="64")] rt_ointer!(a.read(), a, theft, usize, 16);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 1);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 2);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 3);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 4);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 5);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 6);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 7);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 8);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 9);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 10);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 11);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 12);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 13);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 14);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 15);
+            #[cfg(target_pointer_width = "64")]
+            rt_ointer!(a.read(), a, theft, usize, 16);
             drop(Box::from_raw(a));
         }
     })
@@ -172,22 +208,38 @@ fn round_trip_not_null_locals() {
         let b = unsafe { NonNull::new_unchecked(&mut a as *mut usize) };
         let theft = random_usize(&mut rng);
         rt_not_null!(a, b, theft, usize, 0);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 1);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 2);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 3);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 4);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 5);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 6);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 7);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 8);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 9);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 10);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 11);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 12);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 13);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 14);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 15);
-        #[cfg(target_pointer_width="64")] rt_not_null!(a, b, theft, usize, 16);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 1);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 2);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 3);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 4);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 5);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 6);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 7);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 8);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 9);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 10);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 11);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 12);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 13);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 14);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 15);
+        #[cfg(target_pointer_width = "64")]
+        rt_not_null!(a, b, theft, usize, 16);
     })
 }
 
@@ -200,22 +252,38 @@ fn round_trip_not_null_boxes() {
         let theft = random_usize(&mut rng);
         unsafe {
             rt_not_null!(a.read(), b, theft, usize, 0);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 1);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 2);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 3);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 4);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 5);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 6);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 7);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 8);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 9);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 10);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 11);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 12);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 13);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 14);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 15);
-            #[cfg(target_pointer_width="64")] rt_not_null!(a.read(), b, theft, usize, 16);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 1);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 2);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 3);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 4);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 5);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 6);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 7);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 8);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 9);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 10);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 11);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 12);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 13);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 14);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 15);
+            #[cfg(target_pointer_width = "64")]
+            rt_not_null!(a.read(), b, theft, usize, 16);
             drop(Box::from_raw(a));
         }
     })
